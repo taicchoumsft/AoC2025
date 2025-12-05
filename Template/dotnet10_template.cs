b46e6524d5bf756857200928daec7150f31784da
Tai Chou <tai.chou@outlook.com>
2025-12-04 22:12:00 -0800

    simplify template, so far not needed to use any of the helpers

--- Template/dotnet10_template.cs
+++ Template/dotnet10_template.cs
@@ -2,52 +2,21 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Collections.Generic;
 
-
-// Default input file is "input.txt" in project root
 string inputFile = args.Length > 0 ? args[0] : "input.txt";
 var lines = await File.ReadAllLinesAsync(inputFile);
 
 Console.WriteLine("Part 1: " + Part1(lines));
 Console.WriteLine("Part 2: " + Part2(lines));
 
-// Example Part 1 solution stub
 static object Part1(string[] lines)
 {
-    // Use LINQ, parsing helpers, etc.
-    var numbers = lines.Select(int.Parse).ToList();
-    return numbers.Sum();
+    return 0;
 }
 
-// Example Part 2 solution stub
 static object Part2(string[] lines)
 {
-    // Replace with your logic
-    return lines.Length;
-}
-
-// 🔧 Utility helpers
-static IEnumerable<int> ParseInts(IEnumerable<string> lines) =>
-    lines.Select(int.Parse);
-
-static IEnumerable<string[]> SplitLines(IEnumerable<string> lines, char sep = ' ') =>
-    lines.Select(line => line.Split(sep, StringSplitOptions.RemoveEmptyEntries));
-
-static IEnumerable<List<T>> Chunk<T>(IEnumerable<T> source, int size)
-{
-    var chunk = new List<T>(size);
-    foreach (var item in source)
-    {
-        chunk.Add(item);
-        if (chunk.Count == size)
-        {
-            yield return chunk;
-            chunk = new List<T>(size);
-        }
-    }
-    if (chunk.Count > 0)
-        yield return chunk;
+    return 0;
 }
 
